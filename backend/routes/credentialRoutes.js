9814195c5b5c9a84743740a387417ac37f0ef2f0
KaveeshaEkanayaka01 <kavee0106@gmail.com>
2026-04-05 14:13:09 +0530

    Clear main branch

--- backend/routes/credentialRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { verifyCertificatePublic } from "../controllers/credentialController.js";
-
-const router = express.Router();
-
-router.get("/verify/:credentialId", verifyCertificatePublic);
-
-export default router;
